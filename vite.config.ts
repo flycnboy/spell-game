620df8ca6c46dbf41dacd3072fcce2a2eda607ac
flycnboy <flycnboy@gmail.com>
2026-07-05 03:44:41 +0800

    chore: 改为 docs 目录部署 Gitee Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/spell-game/',  // Gitee Pages 路径: 你的用户名.gitee.io/spell-game/
+  base: '/spell-game/',
+  build: {
+    outDir: 'docs',
+  },
 })
